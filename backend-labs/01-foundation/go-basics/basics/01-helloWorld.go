69505c3042e6e7516ed8598400734bf4554b84bf
Harsh Panghal <harshu7850@gmail.com>
2026-04-24 14:40:16 +0530

    chores: initial project setup & adding basics

---
+++ backend-labs/01-foundation/go-basics/basics/01-helloWorld.go
@@ -0,0 +1,35 @@
+//package _name_
+// package declares/describes
+//1. namespace - scope boundary
+
+//2. Compilation unit
+//Go compiles packages, not individual files
+//All files in a package are built together
+
+//3. code organization and reusability
+
+// package main Marks an executable program, Must contain:
+// func main() {}
+
+package basics
+
+// importing necessary packages, modules, libraries, code files etc.
+import "fmt"
+
+// the entry package (package main) requires a main() function.
+// every executable program must have a main method
+// in other files, libraries,and packages, no main() needed
+
+func SayHello() {
+	fmt.Println("Hello World")
+}
+
+// ..... COMMANDS ......
+// to run a Go file :
+// go run    --  go run hello-world.go
+
+// to build the Go file
+// go build   -- go build hello-world.go
+
+// go build will generate a binary executable file that we can run later as executable.
+// ./hello-world
