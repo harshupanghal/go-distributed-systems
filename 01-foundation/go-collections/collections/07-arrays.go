92d3ff7e8f263d6c2342a0c308ab3aff6aec8cf4
Harsh Panghal <harshu7850@gmail.com>
2026-05-04 14:50:28 +0530

    feat: add examples for array(partial-4)

--- 01-foundation/go-collections/collections/07-arrays.go
+++ 01-foundation/go-collections/collections/07-arrays.go
@@ -16,18 +16,29 @@ func Arrays() {
 	// array[index]
 	fmt.Println("value at index 4 : ", a[4])
 
-	// to return lenght of array : use len(array)
-	fmt.Println("lenth of array 'a' : ", len(a))
+	// to return length of array : use len(array)
+	fmt.Println("length of array 'a' : ", len(a))
 
 	// to declare and initialze array in one line
 	b := [5]int{1, 2, 3, 4, 5}
 	fmt.Println("elements of array b: ", b)
 
-	// You can also have the compiler count the number of elements for you with ...
+	// if we omit the size when intializing an array, Go will infer it from the number of elements you provide.
+	// use [...] when not declaring size
 	b = [...]int{1, 2, 3, 4, 5}
 	fmt.Println("dcl:", b)
+
+	// if we specify the size explicitly, the compile will enforce that size.
+	// if a size is specified but elements entered is lower than that, then all remaining elements will be Zero.
+	nums := [3]int{1, 2}
+	fmt.Println(nums)
 
 	// If you specify the index with :, the elements in between will be zeroed.
 	b = [...]int{100, 3: 400, 500}
 	fmt.Println("idx:", b)
+
+	// unlike in other languages like C, if we try to get address of an array, &nums, it doesn't give same result as &nums[0]
+
+	// fmt package will recognizw it as a pointer to an array and shows the array's contents (&[1 2 3]) rather than a raw memory address.
+
 }
