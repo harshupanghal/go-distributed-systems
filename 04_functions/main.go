9173a29012548b7c04774ff2ead1030a57a507f0
Harsh Panghal <harshu7850@gmail.com>
2026-05-07 21:23:56 +0530

    Add examples for multiple return values

--- 04_functions/main.go
+++ 04_functions/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	functions "prep/04_functions/functions"
+	returnValues "prep/04_functions/multiple-return-values"
 )
 
 func main() {
 	functions.Functions()
+	returnValues.MutipleReturnValues()
 }
